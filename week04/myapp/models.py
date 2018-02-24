25f235071527d79d9b3f4ff3f35e628c2a9cb44f
CyberNueng <nueng.42560@gmail.com>
2018-02-25 00:46:41 +0700

    add

--- week04/myapp/models.py
+++ week04/myapp/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Car(models.Model):
+    model=models.CharField(max_length=30)
+    detail=models.CharField(max_length=100)
+    price=models.DecimalField(max_digits=10,decimal_places=2,default=0.00)
+
+class Rent(models.Model):
+    user=models.ForeignKey(User, on_delete=models.CASCADE)
+    car=models.ForeignKey(Car, on_delete=models.CASCADE)
+    start_datetime=models.DateTimeField()
+    return_time=models.DateTimeField()
+    fee=models.DecimalField(max_digits=10,decimal_places=2,default=0.00)
